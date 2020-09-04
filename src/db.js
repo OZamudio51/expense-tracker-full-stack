c1431c569cf3fad18b0f0af358459eceeef08a75
Oscar Zamudio <zamudio_1526@hotmail.com>
2020-09-04 11:31:35 -0700

    changes

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect('mongodb+srv://dunder_mifflin:paper@expense-tracker-db.pljzj.mongodb.net/expense-tracker-db?retryWrites=true&w=majority', {
             useNewUrlParser: true,
             useCreateIndex: true, 
             useUnifiedTopology: true
