c30cb052b25acd88640ad3628d4557b60970886d
Oscar Zamudio <zamudio_1526@hotmail.com>
2020-10-06 09:14:49 -0700

    Add client folder back to git repo

---
+++ client/build/precache-manifest.a6eaa9376e50ce15a233f0fa719f852f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d345b9135778d77aedb5a937d623760",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb5e7885cc2b1cf4cd20",
+    "url": "/static/css/main.c5ee3396.chunk.css"
+  },
+  {
+    "revision": "ef9395b1b43e8d7649bd",
+    "url": "/static/js/2.7b6482cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7b6482cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb5e7885cc2b1cf4cd20",
+    "url": "/static/js/main.aa35448b.chunk.js"
+  },
+  {
+    "revision": "1eede93b43bca28572ca",
+    "url": "/static/js/runtime-main.399baf1c.js"
+  }
+]);
---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import  { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import {  TransactionsList } from './components/TransactionsList'; 
+import {  AddTransaction } from './components/AddTransaction'; 
+import { GlobalProvider } from './Context/GlobalState';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionsList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
